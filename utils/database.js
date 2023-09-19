c7f2bf83b7ffc84777af6c849ef0403453c9450b
Abhilash M <abhilash.m@skein.com>
2023-09-19 13:05:24 +0530

    database configured

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'signup_app'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 db.connect(function(err){
